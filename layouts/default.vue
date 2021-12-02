7ff9ed7e73c9140b7d3e9d0c2051c9c0b4d3631b
MatioZG <matheus.fongaro@gmail.com>
2021-12-02 17:38:51 -0400

    build: criação do select de tipos

--- layouts/default.vue
+++ layouts/default.vue
@@ -52,7 +52,7 @@ export default {
   },
   methods: {
     goBack () {
-      this.$router.go(-1)
+      this.$router.go({ name: 'IndexPage' })
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,69 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-row
+    justify="space-around"
+    align="start"
+    class="search-row"
+    no-gutters
+  >
+    <v-col md="3">
+      <div>
+        <div class="mb-0 ml-1 primary--text">
+          Filtrar por:
+        </div>
+        <v-select
+          v-model="filtroSelecionado"
+          class="mt-0"
+          :items="tiposDeFiltro"
+          label="Escolha uma opção"
+        />
+      </div>
+    </v-col>
+    <v-col md="3">
+      <p v-if="filtroSelecionado != null">
+        Outra coisa
+      </p>
+    </v-col>
+    <v-col md="3">
+      <CommonRegularButton :btn-text="'Pesquisar'" />
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      tiposDeFiltro: [
+        {
+          text: 'Região',
+          value: 'region'
+        },
+        {
+          text: 'Capital',
+          value: 'capital'
+        },
+        {
+          text: 'Língua',
+          value: 'lang'
+        },
+        {
+          text: 'País',
+          value: 'country'
+        },
+        {
+          text: 'Código de ligação',
+          value: 'code'
+        }
+      ],
+      filtroSelecionado: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-row {
+  margin-top: 218px;
+}
+</style>
