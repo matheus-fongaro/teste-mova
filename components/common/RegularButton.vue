61e8738f03940a9ca3b294ccb1a9f5e944861db2
MatioZG <matheus.fongaro@gmail.com>
2021-12-02 17:38:08 -0400

    build: criação do regular button

---
+++ components/common/RegularButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn color="primary" class="px-3 text-capitalize">
+    {{ btnText }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'RegularButton',
+  props: {
+    btnText: {
+      type: String,
+      default: 'Ok'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-btn {
+  font-size: 14px;
+  font-weight: 400;
+}
+</style>
