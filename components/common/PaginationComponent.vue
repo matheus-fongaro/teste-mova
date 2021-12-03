829751030e1b5bd61a6c1870fbe6ba99d0a4444c
MatioZG <matheus.fongaro@gmail.com>
2021-12-03 15:07:51 -0400

    build: pagination component

---
+++ components/common/PaginationComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center">
+    <v-pagination
+      v-model="pagina"
+      color="primary"
+      :length="tamanho"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaginationComponent',
+  props: {
+    tamanho: {
+      type: Number,
+      required: true
+    }
+  },
+  data () {
+    return {
+      pagina: 1
+    }
+  },
+  watch: {
+    pagina (val) {
+      this.$emit('pagina', val)
+    }
+  }
+}
+</script>
