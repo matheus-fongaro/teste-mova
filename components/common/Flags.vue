bfa8378fea694b478bca35bc1e057fae9b6cf835
MatioZG <matheus.fongaro@gmail.com>
2021-12-03 16:26:58 -0400

    build: flags component

---
+++ components/common/Flags.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col v-for="n in 3" :key="n" class="d-flex child-flex" cols="12" md="4">
+      <v-img src="https://picsum.photos/316/181" lazy-src="https://picsum.photos/7/4">
+        <template #placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            />
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
+  </v-row>
+</template>
