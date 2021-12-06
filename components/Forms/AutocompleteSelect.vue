fd62732bdc8984c6fa82c10d7dc7a5a1def500b9
MatioZG <matheus.fongaro@gmail.com>
2021-12-06 14:50:29 -0400

    build: autocomplete component

---
+++ components/Forms/AutocompleteSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="mb-2 ml-1 primary--text text-capitalize">
+      {{ label }}
+    </div>
+    <v-autocomplete
+      v-model="autocomplete"
+      class="mt-0 pt-0 weight-700"
+      :items="items"
+      :label="hint"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AutocompleteSelect',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    hint: {
+      type: String,
+      default: 'Selecione uma opção'
+    }
+  },
+  data () {
+    return {
+      autocomplete: ''
+    }
+  }
+}
+</script>
