a61c02f06b2a99d840c25a98b39014796613a539
MatioZG <matheus.fongaro@gmail.com>
2021-12-07 11:43:28 -0400

    build: componentizei a imagem da bandeira

---
+++ components/common/Flags/FlagComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-img :src="country.flag" :width="maxWidth" alt="Bandeira do país" @click="details(country)">
+    <template #placeholder>
+      <v-row
+        class="fill-height ma-0"
+        align="center"
+        justify="center"
+      >
+        <v-progress-circular
+          indeterminate
+          color="grey lighten-5"
+        />
+      </v-row>
+    </template>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: 'FlagComponent',
+  props: {
+    country: {
+      type: Object,
+      required: true
+    },
+    maxWidth: {
+      type: String,
+      default: '100px'
+    }
+  },
+  data () {
+    return {
+      borderCountries: []
+    }
+  },
+  methods: {
+    details (country) {
+      this.$emit('details', country)
+    }
+  }
+}
+</script>
