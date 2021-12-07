3dc62002ba906708c2255c56380600a37d13d0c3
MatioZG <matheus.fongaro@gmail.com>
2021-12-07 11:44:00 -0400

    refactor: mudanças de componentização

--- components/common/Flags.vue
+++ components/common/Flags.vue
@@ -1,20 +1,7 @@
 <template>
   <v-row>
     <v-col v-for="country in countries" :key="country.ccn3" class="d-flex child-flex" cols="12" md="4">
-      <v-img :src="country.flags.png" max-width="316">
-        <template #placeholder>
-          <v-row
-            class="fill-height ma-0"
-            align="center"
-            justify="center"
-          >
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            />
-          </v-row>
-        </template>
-      </v-img>
+      <CommonFlagsFlagComponent :country="country" @details="details" />
     </v-col>
   </v-row>
 </template>
@@ -26,6 +13,11 @@ export default {
     countries: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    details (country) {
+      this.$router.push({ name: 'Details', params: { country } })
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <FormsSearchByType :class="{ 'mt-2' : $vuetify.breakpoint.xs, 'mt-15' : $vuetify.breakpoint.smAndUp}" @pesquisar="pesquisar" />
     </div>
     <div>
-      <CommonFlags class="mt-2 mb-auto" :countries="paginatedCountries" />
+      <CommonFlags class="mt-2 mb-auto" :countries="paginatedCountries" :max-width="'316'" />
     </div>
     <div class="mt-auto">
       <CommonPaginationComponent :tamanho="tamanho" @pagina="paginacao" />
@@ -28,16 +28,13 @@ export default {
     }
   },
   async mounted () {
-    const data = await this.$axios.$get('https://restcountries.com/v3.1/all')
+    const data = await this.$axios.$get('https://restcountries.com/v2/all')
     this.countries = data
     data.length > 1 ? this.tamanho = Math.ceil(data.length / 12) : this.tamanho = 1
   },
   methods: {
     async pesquisar (pesquisa) {
-      // as duas versões da api tem variáveis com nomes diferentes, e a versão 3.1 não tem o calling code do
-      // jeito que preciso, então por isso precisei usar as duas versões, dependendo da requisição
-      const version = pesquisa.selectedType === 'callingcode' ? 'v2' : 'v3.1'
-      const data = await this.$axios.$get(`https://restcountries.com/${version}/${pesquisa.selectedType}/${pesquisa.termo}`)
+      const data = await this.$axios.$get(`https://restcountries.com/v2/${pesquisa.selectedType}/${pesquisa.termo}`)
       this.pagina = 1
       data.length > 1 ? this.tamanho = Math.ceil(data.length / 12) : this.tamanho = 1
       this.countries = data
