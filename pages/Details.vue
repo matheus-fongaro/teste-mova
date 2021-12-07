7d54fad16a5aec52d289358f0b5d14160a562b83
MatioZG <matheus.fongaro@gmail.com>
2021-12-07 11:43:45 -0400

    build: criei a página de detalhes

---
+++ pages/Details.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <v-row class="mt-lg-12">
+      <v-col
+        cols="12"
+        md="5"
+      >
+        <CommonFlagsFlagComponent
+          :country="country"
+          :max-width="'443'"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <p>Nome: {{ country.translations.br }}</p>
+        <p>Capital: {{ country.capital }}</p>
+        <p>Região: {{ country.region }}</p>
+        <p>Sub-região: {{ country.subregion }}</p>
+        <p>População: {{ country.population }}</p>
+        <div
+          v-for="lingua of country.languages"
+          :key="lingua.nome"
+        >
+          <p>Línguas: {{ lingua.name }}</p>
+        </div>
+      </v-col>
+    </v-row>
+    <div v-if="hasBorderCountries">
+      <p class="mt-lg-15">
+        Países Vizinhos:
+      </p>
+      <v-row>
+        <v-col
+          v-for="paisVizinho of borderCountries"
+          :key="paisVizinho.alpha3Code"
+          cols="12"
+          md="4"
+        >
+          <CommonFlagsFlagComponent
+            :country="paisVizinho"
+            :max-width="'316'"
+            @details="changeCountry"
+          />
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else class="mt-lg-15">
+      <strong>Este país não faz fronteira com nenhum outro.</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Details',
+  data () {
+    return {
+      country: {},
+      borderCountriesArray: [],
+      borderCountries: [],
+      hasBorderCountries: false
+    }
+  },
+  created () {
+    this.country = this.$route.params.country
+  },
+  async mounted () {
+    if (this.country.borders) {
+      this.borderCountriesArray = Object.values(this.$route.params.country.borders).join(',')
+      this.borderCountries = await this.$axios.$get(`https://restcountries.com/v2/alpha?codes=${this.borderCountriesArray}`)
+      this.hasBorderCountries = true
+    } else {
+      this.hasBorderCountries = false
+    }
+  },
+  methods: {
+    async changeCountry (country) {
+      this.country = country
+      if (this.country.borders) {
+        this.borderCountriesArray = Object.values(this.country.borders).join(',')
+        this.borderCountries = await this.$axios.$get(`https://restcountries.com/v2/alpha?codes=${this.borderCountriesArray}`)
+        this.hasBorderCountries = true
+      } else {
+        this.hasBorderCountries = false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+p, strong {
+  font-size: 18px;
+}
+</style>
